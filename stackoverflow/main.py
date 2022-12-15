9ba1d9932059b7df31e9c47fd64401849b56eed3
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-12-15 23:41:48 +0000

    Include dbt project

--- stackoverflow/main.py
+++ stackoverflow/main.py
@@ -15,7 +15,7 @@ def handler(*args, **kwargs):  # pylint: disable=unused-argument
     content = requests.get(URL, timeout=None).content
 
     data = pd.read_csv(io.StringIO(content.decode())).convert_dtypes()
-    data["loaded_at"] = datetime.datetime.now().isoformat()
+    data["loaded_at"] = datetime.datetime.now()
 
     data.to_gbq(destination_table="fivethirtyeight.spi", if_exists="append")
     return {"statusCode": 200, "message": f"loaded {data.shape[0]} rows"}
