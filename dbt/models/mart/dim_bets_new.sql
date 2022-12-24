66a0aece608609c92ae7a3e6d114f38f4205906f
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-12-24 18:07:36 -0300

    Update dim_bets_new.sql

--- dbt/models/mart/dim_bets_new.sql
+++ dbt/models/mart/dim_bets_new.sql
@@ -4,4 +4,4 @@ FROM
   {{ ref("dim_bets") }} AS b
   LEFT JOIN {{ ref("stg_sent") }} AS s ON b.user = s.user AND b.id = s.id
 WHERE
-  round(b.ev, 3) > fnull(round(s.ev, 3), 0)
+  round(b.ev, 3) > ifnull(round(s.ev, 3), 0)
