69529b8d26c3a6a1125403f3df4fd89197c08858
Matheus Couto <matheusccouto@gmail.com>
2022-12-29 15:21:25 -0300

    List bookmakers

--- telegram-config/main.py
+++ telegram-config/main.py
@@ -9,27 +9,23 @@ import telegram
 
 client = google.cloud.logging.Client()
 client.setup_logging()
+bot = telegram.Bot(os.getenv("TELEGRAM_TOKEN"))
 
 
 def handler(request):
     """Send telegrams messages."""
-    bot = telegram.Bot(os.getenv("TELEGRAM_TOKEN"))
 
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(), bot)
-        chat_id = update.message.chat.id
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    if request.method != "POST":
+        return {"statusCode": 200}
 
-    # query = """
-    #     SELECT
-    #         *,
-    #         date(start_at, 'America/Sao_Paulo') AS date
-    #     FROM
-    #         tipster.fct_tips
-    #     WHERE
-    #         user = 'tipster'
-    # """
-    # data = pd.read_gbq(query=query)
+    update = telegram.Update.de_json(request.get_json(), bot)
+    chat_id = update.message.chat.id
+
+    if "/listbookmakers" in update.message.text:
+        query = f"SELECT bookmaker FROM tipster.user_bookmaker WHERE user = {chat_id}"
+        text = "\n".join(pd.read_gbq(query=query)["bookmaker"])
+        bot.sendMessage(chat_id=chat_id, text=text)
+        return {"statusCode": 200}
 
     # sent.to_gbq("tipster.sent", if_exists="append")
 
