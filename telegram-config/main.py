37962baf845ab78ae77bf20b7c5195d4b0760bf9
Matheus Couto <matheusccouto@gmail.com>
2022-12-29 16:43:08 -0300

    Update user type

--- telegram-config/main.py
+++ telegram-config/main.py
@@ -1,7 +1,6 @@
 """Telegram bot for sending betting tips."""
 
 import os
-from datetime import datetime
 
 import google.cloud.logging
 import pandas as pd
@@ -25,7 +24,7 @@ def handler(request):
         query = f"""
             SELECT bookmaker
             FROM tipster.user_bookmaker
-            WHERE user = '{chat_id}'
+            WHERE user = {chat_id}
             ORDER BY bookmaker
             """
         text = "\n".join(pd.read_gbq(query=query)["bookmaker"])
