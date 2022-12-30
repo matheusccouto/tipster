fd011b2857e5b107711bdbc97d50e663ce03d4f1
Matheus Couto <matheusccouto@gmail.com>
2022-12-30 13:00:30 -0300

    Update read choice

--- telegram-config/main.py
+++ telegram-config/main.py
@@ -77,13 +77,11 @@ def _read_choice(chat_id, text, query_list, query_update):
         selected = list(data)[int(text)]
     except ValueError:
         bot.sendMessage(chat_id=chat_id, text="Type only the number")
-        return {"statusCode": 200}
     except IndexError:
         bot.sendMessage(chat_id=chat_id, text="Type a number from the list")
-        return {"statusCode": 200}
-    run_query(query_update.format(chat_id=chat_id, key=selected.key))
-    context[chat_id] = None
-    return selected
+    finally:
+        run_query(query_update.format(chat_id=chat_id, key=selected.key))
+        return selected
 
 
 def add(chat_id, text, query_available, query_set):
@@ -101,7 +99,6 @@ def list_(chat_id, query):
     text = "\n".join([row.name for row in data])
     text = text if text else "You do not have bookmakers"
     bot.sendMessage(chat_id=chat_id, text=text)
-    context[chat_id] = None
 
 
 def handler(request):
@@ -130,8 +127,9 @@ def handler(request):
     # Get user answer when setting a new bookie.
     if context.get(chat_id) == "/setbookmaker":
         add(chat_id, text, QUERY_AVAILABLE_BOOKIE, QUERY_SET_BOOKIE)
+        context[chat_id] = None
         return {"statusCode": 200}
-    
+
     # List bookmakers that could be deleted
     if "/deletebookmaker" in text:
         context[chat_id] = "/deletebookmaker"
@@ -141,6 +139,7 @@ def handler(request):
     # Get user answer when setting a new bookie.
     if context.get(chat_id) == "/deletebookmaker":
         delete(chat_id, text, QUERY_AVAILABLE_BOOKIE, QUERY_SET_BOOKIE)
+        context[chat_id] = None
         return {"statusCode": 200}
 
     # List user's bookmakers
