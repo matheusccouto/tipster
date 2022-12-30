22683b0c0619bef9d2ce50a65da1329655966b3b
Matheus Couto <matheusccouto@gmail.com>
2022-12-30 14:23:27 -0300

    Update stg_user_league

--- dbt/models/staging/stg_user_league.sql
+++ dbt/models/staging/stg_user_league.sql
@@ -4,5 +4,5 @@ SELECT
     concat(f.emoji, ' ', l.tipster) AS name,  -- noqa: L029
 FROM
     {{ ref("user_league") }} AS u
-LEFT JOIN {{ ref("league") }} AS l ON u.league = b.id
+LEFT JOIN {{ ref("league") }} AS l ON u.league = l.id
 LEFT JOIN {{ ref("flag") }} AS f on l.country = f.country
