4df488d7b4e8f1f46c0fcf7a55cc1d6fad3c42ff
Matheus Couto <matheusccouto@gmail.com>
2022-12-30 14:00:43 -0300

    Update user_bookmaker

--- dbt/models/staging/stg_user_bookmaker.sql
+++ dbt/models/staging/stg_user_bookmaker.sql
@@ -1,7 +1,7 @@
 SELECT
     u.user,
     b.key,
-    b.bookmaker AS name  -- noqa: L029
+    b.name
 FROM
     {{ ref("user_bookmaker") }} AS u
 LEFT JOIN {{ ref("bookmaker") }} AS b ON u.bookmaker = b.key
