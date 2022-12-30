14e6a2f80c8a861b7075c68ed604459b55ed5848
Matheus Couto <matheusccouto@gmail.com>
2022-12-30 11:58:59 -0300

    Add delete

--- telegram-config/main.py
+++ telegram-config/main.py
@@ -35,23 +35,14 @@ QUERY_SET_BOOKIE = """
     VALUES ({chat_id}, '{key}')
 """
 QUERY_LIST_BOOKIE = """
-    SELECT bookmaker AS name
+    SELECT bookmaker AS key, bookmaker AS name
     FROM tipster.user_bookmaker
     WHERE user = {chat_id}
     ORDER BY bookmaker
     """
-QUERY_AVAILABLE_LEAGUE = """
-    SELECT b.key, b.name
-    FROM tipster.bookmaker AS b
-    LEFT JOIN tipster.user_bookmaker AS u ON b.key = u.bookmaker AND user = {chat_id}
-    WHERE u.user IS NULL
-    ORDER BY b.name
-"""
-QUERY_LIST_LEAGUE = """
-    SELECT bookmaker
-    FROM tipster.user_bookmaker
-    WHERE user = {chat_id}
-    ORDER BY bookmaker
+QUERY_DELETE_BOOKIE = """
+    DELETE FROM tipster.user_bookmaker
+    WHERE user = {chat_id} AND bookmaker = '{key}'
 """
 
 # General config.
@@ -73,28 +64,39 @@ def run_query(query):
     return job.result()
 
 
-def list_available(chat_id, query):
+def choices(chat_id, query):
     data = run_query(query.format(chat_id=chat_id))
     text = "\n".join([f"{i}. {row.name}" for i, row in enumerate(data)])
     text = f"Select a number from the list\n\n{text}"
     bot.sendMessage(chat_id=chat_id, text=text)
 
 
-def add_new(chat_id, text, query_available, query_set):
-    data = run_query(query_available.format(chat_id=chat_id))
+def _read_choice(chat_id, text, query_list, query_update):
+    data = run_query(query_list.format(chat_id=chat_id))
     try:
         selected = list(data)[int(text)]
     except ValueError:
         bot.sendMessage(chat_id=chat_id, text="Type only the number")
+        return {"statusCode": 200}
     except IndexError:
         bot.sendMessage(chat_id=chat_id, text="Type a number from the list")
-    finally:
-        run_query(query_set.format(chat_id=chat_id, key=selected.key))
-        bot.sendMessage(chat_id=chat_id, text=f"Added {selected.name}")
+        return {"statusCode": 200}
+    run_query(query_update.format(chat_id=chat_id, key=selected.key))
     context[chat_id] = None
+    return selected
 
 
-def list_current(chat_id, query):
+def add(chat_id, text, query_available, query_set):
+    selected = _read_choice(chat_id, text, query_available, query_set)
+    bot.sendMessage(chat_id=chat_id, text=f"Added {selected.name}")
+
+
+def delete(chat_id, text, query_current, query_delete):
+    selected = _read_choice(chat_id, text, query_current, query_delete)
+    bot.sendMessage(chat_id=chat_id, text=f"Deleted {selected.name}")
+
+
+def list_(chat_id, query):
     data = run_query(query.format(chat_id=chat_id))
     text = "\n".join([row.name for row in data])
     text = text if text else "You do not have bookmakers"
@@ -122,17 +124,28 @@ def handler(request):
     # Show available bookies if the user wants to set a new one.
     if "/setbookmaker" in text:
         context[chat_id] = "/setbookmaker"
-        list_available(chat_id, QUERY_AVAILABLE_BOOKIE)
+        choices(chat_id, QUERY_AVAILABLE_BOOKIE)
         return {"statusCode": 200}
 
     # Get user answer when setting a new bookie.
     if context.get(chat_id) == "/setbookmaker":
-        add_new(chat_id, text, QUERY_AVAILABLE_BOOKIE, QUERY_SET_BOOKIE)
+        add(chat_id, text, QUERY_AVAILABLE_BOOKIE, QUERY_SET_BOOKIE)
+        return {"statusCode": 200}
+    
+    # List bookmakers that could be deleted
+    if "/deletebookmaker" in text:
+        context[chat_id] = "/deletebookmaker"
+        choices(chat_id, QUERY_LIST_BOOKIE)
+        return {"statusCode": 200}
+
+    # Get user answer when setting a new bookie.
+    if context.get(chat_id) == "/deletebookmaker":
+        delete(chat_id, text, QUERY_AVAILABLE_BOOKIE, QUERY_SET_BOOKIE)
         return {"statusCode": 200}
 
     # List user's bookmakers
     if "/listbookmakers" in text:
-        list_current(chat_id, QUERY_LIST_BOOKIE)
+        list_(chat_id, QUERY_LIST_BOOKIE)
         return {"statusCode": 200}
 
     welcome_msg = "You can control me by sending these commands:"
