1fa85eeb799134790b062ae66185826b9336ef8c
Matheus Couto <matheusccouto@gmail.com>
2023-01-02 11:58:26 -0300

    Remove cycle

--- dbt/models/staging/stg_user_league.sql
+++ dbt/models/staging/stg_user_league.sql
@@ -3,6 +3,6 @@ SELECT
     l.id AS key,  -- noqa: L029
     concat(f.emoji, ' ', l.tipster) AS name,  -- noqa: L029
 FROM
-    {{ ref("stg_user_league") }} AS u
+    {{ ref("user_league") }} AS u
 LEFT JOIN {{ ref("league") }} AS l ON u.league = l.id
 LEFT JOIN {{ ref("flag") }} AS f on l.country = f.country
