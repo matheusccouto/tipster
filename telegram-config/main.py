cfecf4e31e5e8c6a7a6209a4774a7033a3ecf900
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2023-01-04 20:36:17 +0000

    Avoid newlines

--- telegram-config/main.py
+++ telegram-config/main.py
@@ -152,7 +152,8 @@ def list_(chat_id, query):
 
 def set_value(chat_id, query, value):
     """Set a value."""
-    run_query(query.format(chat_id=chat_id, value=emoji.demojize(value)))
+    value = emoji.demojize(value).replace("\n", "\\n")
+    run_query(query.format(chat_id=chat_id, value=value))
     context[chat_id] = None
     bot.sendMessage(chat_id, text=f"Set {value}")
 
