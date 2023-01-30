dc58da11ecb759955f25db7b0336535ca764b9b3
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2023-01-30 23:12:19 +0000

    Update backtesting app

--- backtesting/main.py
+++ backtesting/main.py
@@ -14,7 +14,9 @@ from google.cloud import bigquery
 THIS_DIR = os.path.dirname(__file__)
 TODAY = datetime.datetime.today()
 
-creds = service_account.Credentials.from_service_account_info(info=st.secrets["gcp_service_account"])
+creds = service_account.Credentials.from_service_account_info(
+    info=st.secrets["gcp_service_account"]
+)
 client = bigquery.Client(credentials=creds)
 
 
@@ -37,12 +39,12 @@ def run_query(query_):
 def transform_data(dataframe):
     """Transform data to candle bars plot"""
     return (
-        dataframe
-        .sort_values("start_at")
+        dataframe.sort_values("start_at")
         .set_index("start_at")["result"]
         .cumsum()
         .resample("D")
         .agg(["first", "last", "max", "min"])
+        .round(2)
         .reset_index()
         .rename(
             columns={
@@ -60,7 +62,7 @@ def format_text(text):
     """Format text with newlines and emojis"""
     text = text.replace(":England:", ":United_Kingdom:")
     text = text.replace(":Scotland:", ":United_Kingdom:")
-    text=text.replace("\\n", "\n")
+    text = text.replace("\\n", "\n")
     return emoji.emojize(text)
 
 
@@ -84,98 +86,72 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    st.title("Tipster Backtesting")
-
-
-    # Load sidebar options.
+    st.title("Tipster")
 
     bookmakers_query = """SELECT key, name FROM tipster.stg_bookmaker ORDER BY name"""
-    all_bookmakers = run_query(bookmakers_query).set_index("key")["name"].to_dict()
-
     leagues_query = "SELECT key, name FROM tipster.stg_league ORDER BY key"
+    all_bookmakers = run_query(bookmakers_query).set_index("key")["name"].to_dict()
     all_leagues = run_query(leagues_query).set_index("key")["name"].to_dict()
 
+    data = run_query("SELECT * FROM tipster.fct_bet WHERE outcome IS NOT NULL")
 
-    # Sidebar
+    with st.form("my_form"):
+        user = st.selectbox("User", options=data["user"].unique())
+        data = data[data["user"] == user]
 
-    sbl, sbr = st.sidebar.columns(2)
+        date_range = pd.date_range(
+            start=data["start_at"].min(),
+            end=data["start_at"].max(),
+        ).date
+        dates = st.select_slider(
+            "Date range",
+            value=[date_range.min(), date_range.max()],
+            options=date_range,
+        )
+        st.form_submit_button("Submit")
 
-    start_date = sbl.date_input("Start", value=TODAY - datetime.timedelta(days=180), max_value=TODAY)
-    end_date = sbr.date_input("End", value=TODAY, min_value=start_date, max_value=TODAY)
+    data = data[
+        (data["start_at"].dt.date >= dates[0]) & (data["start_at"].dt.date <= dates[1])
+    ]
 
-    ev = sbl.number_input(
-        "EV Threshold",
-        value=0.0,
-        min_value=0.0,
-        step=0.01,
-    )
-    factor = sbr.number_input(
-        "Kelly Criterion Factor",
-        value=1.0,
-        min_value=0.0,
-        max_value=1.0,
-        step=0.01,
-    )
-    bookmakers = st.sidebar.multiselect(
-        "Bookmakers",
-        options=all_bookmakers,
-        format_func=lambda x: format_text(all_bookmakers[x]),
-        default=all_bookmakers,
-    )
-    leagues = st.sidebar.multiselect(
-        "Leagues",
-        options=all_leagues,
-        format_func=lambda x: format_text(all_leagues[x]),
-        default=all_leagues,
-    )
-
-
-    if not bookmakers or not leagues:
-        st.stop()
-
-
-    query = read_query(os.path.join(THIS_DIR, "query.sql"))
-    query = query.format(
-        bookmakers="','".join([str(b) for b in bookmakers]),
-        leagues=",".join([str(l) for l in leagues]),
-        factor=factor,
-        ev=ev,
-        kelly="{kelly}",
-        start=start_date,
-        end=end_date,
-    )
-    data = run_query(query)
     data["message"] = data["message"].apply(format_text)
     candles = transform_data(data)
 
-    kpi_cols = st.columns(4)
+    with st.expander("KPIs", expanded=True):
 
-    kpi = calculate_kpi(data)
-    kpi_cols[0].metric("Net", f"${kpi['net']:.2f}")
-    kpi_cols[1].metric("ROI", f"{100 * kpi['roi']:.1f}%")
-    kpi_cols[2].metric("Investment", f"${kpi['investment']:.2f}")
-    kpi_cols[3].metric("Prizes", f"${kpi['prize']:.2f}")
+        kpi_cols = st.columns(4)
 
-    fig = go.Figure(
-        data=[
-            go.Candlestick(
-                x=candles["date"],
-                open=candles["open"],
-                high=candles["high"],
-                low=candles["low"],
-                close=candles["close"],
-            )
-        ]
+        kpi = calculate_kpi(data)
+        kpi_cols[0].metric("Net", f"${kpi['net']:.2f}")
+        kpi_cols[1].metric("ROI", f"{100 * kpi['roi']:.1f}%")
+        kpi_cols[2].metric("Investment", f"${kpi['investment']:.2f}")
+        kpi_cols[3].metric("Prizes", f"${kpi['prize']:.2f}")
+
+    with st.expander("Candles", expanded=True):
+
+        fig = go.Figure(
+            data=[
+                go.Candlestick(
+                    x=candles["date"],
+                    open=candles["open"],
+                    high=candles["high"],
+                    low=candles["low"],
+                    close=candles["close"],
+                )
+            ]
+        )
+        fig.update_layout(
+            margin=dict(l=20, r=20, t=20, b=20), xaxis_rangeslider_visible=False
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+    data["Bookmaker"] = data["bookmaker_key"].apply(
+        lambda x: format_text(all_bookmakers[x])
     )
-    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20), xaxis_rangeslider_visible=False)
-    st.plotly_chart(fig, use_container_width=True)
-
-
-    data["Bookmaker"] = data["bookmaker_key"].apply(lambda x: format_text(all_bookmakers[x]))
     data["League"] = data["league_id"].apply(lambda x: format_text(all_leagues[x]))
     data["Net"] = data["result"]
 
-    with st.expander("Cats"):
+    with st.expander("Cats", expanded=True):
 
         cats_left, cats_right = st.columns(2)
 
@@ -204,8 +180,7 @@ def main():
         )
         cats_left.plotly_chart(fig, use_container_width=True)
 
-
-    with st.expander("Bets"):
+    with st.expander("Bets", expanded=True):
         for _, row in data.sort_values("start_at", ascending=False).iterrows():
             message = row["message"].replace("$", "\$")
             result = round(row["result"], 2)
